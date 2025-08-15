2413f1dda7531c7770351125a3c84bd4f9a5019a
abakm <akhilbalakrishnanakm@gmail.com>
2025-08-15 15:55:39 +0530

    change origins

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://shopgenie-ui-production.up.railway.app"],  # Your React app URL
+    allow_origins=[ "*"],  # Your React app URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
